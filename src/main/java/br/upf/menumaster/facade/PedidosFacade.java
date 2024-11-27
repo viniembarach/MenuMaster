fad8441c59f2d2a594b6522357fbe6d7b1a395ac
Marcos Vinicius Bedin Embarach <viniembarach12@gmail.com>
2024-11-26 21:30:39 -0300

    Busca Cozinha ajustada

--- src/main/java/br/upf/menumaster/facade/PedidosFacade.java
+++ src/main/java/br/upf/menumaster/facade/PedidosFacade.java
@@ -64,12 +64,11 @@ public class PedidosFacade extends AbstractFacade<Pedidos> {
         }
         return pedidosComStatus;
     }
-    
+
     public List<Pedidos> buscarPedidosCozinha() {
         List<Pedidos> pedidosCozinha = new ArrayList<>();
         try {
-            // Consulta JPQL para buscar pedidos onde o status de pagamento não é nulo
-            Query query = getEntityManager().createQuery("SELECT p FROM Pedidos p WHERE p.statuspagamento = 'NAO_PAGO' ORDER BY p.idpedido");
+            Query query = getEntityManager().createQuery("SELECT p FROM Pedidos p WHERE p.statuspagamento = 'Não Pago' AND p.statuspedido <> 'Pronto' ORDER BY p.idpedido");
             pedidosCozinha = query.getResultList();
         } catch (Exception e) {
             System.out.println("Erro: " + e);
